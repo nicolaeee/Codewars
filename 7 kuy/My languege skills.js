e4563f3304dded230f8cb60e599484fd0736287c
onyxgeek <onyxpeek@gmail.com>
2023-03-06 20:40:11 +0200

    feat:finish the another task

--- 7 kuy/My languege skills.js
+++ 7 kuy/My languege skills.js
@@ -1,9 +1,23 @@
-let results = {"Java": 10, "Ruby": 80, "Python": 65},
+var results = {"Java": 10, "Ruby": 80, "Python": 65},
     arr = [];
+
+
+
 
 for (let key in results) {
    if(results[key] >= 60) {
-    arr.push(key);
+    arr.push(key)
+    console.log(arr);
    }
   };
-  console.log(arr)
+
+
+//   let a = {b: 60, c : 93, d: 71}
+
+//   let A = {c: 93, d: 71, b: 60}
+
+// let result = arr.push(results);
+
+// var list = {"you": 100, "me": 75, "foo": 116, "bar": 15};
+// keysSorted = Object.keys(list).sort(function(a,b){return list[a]-list[b]})
+// console.log(keysSorted);
--- 7 kuy/tempCodeRunnerFile.js
+++ 7 kuy/tempCodeRunnerFile.js
@@ -1,10 +1,12 @@
-let results = {"Java": 10, "Ruby": 80, "Python": 65},
+var results = {"Java": 10, "Ruby": 80, "Python": 65},
     arr = [];
+
+
+
 
 for (let key in results) {
    if(results[key] >= 60) {
-    arr.push(key);
-
+    arr.push(key)
+    console.log(arr);
    }
   };
-  console.log(arr)
