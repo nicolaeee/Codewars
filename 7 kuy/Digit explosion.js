3200d6084346e1cb97b4305b941287d5a086c0b1
onyxgeek <onyxpeek@gmail.com>
2023-03-06 21:19:41 +0200

    upd:add new desicion for this problem which I can handle

--- 7 kuy/Digit explosion.js
+++ 7 kuy/Digit explosion.js
@@ -11,3 +11,30 @@ function explode(a) {
   };
 
 console.log(explode(input));
+
+function explode(s) {
+    let arr = [];
+    let split = s.split("");
+    for (let item of s) {
+      if (item == "1") {
+        arr.push("1");
+      } else if (item == "2") {
+        arr.push("22");
+      } else if (item == "3") {
+        arr.push("333");
+      } else if (item == "4") {
+        arr.push("4444");
+      } else if (item == "5") {
+        arr.push("55555");
+      } else if (item == "6") {
+        arr.push("666666");
+      } else if (item == "7") {
+        arr.push("7777777");
+      } else if (item == "8") {
+        arr.push("88888888");
+      } else if (item == "9") {
+        arr.push("999999999");
+      }
+    }
+    return arr.join("");
+  }
