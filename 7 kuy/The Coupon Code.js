6fd47b4db63f5f16a6286d2c22b399ff777f78b6
onyxgeek <onyxpeek@gmail.com>
2023-03-08 19:58:07 +0200

    update: add some comments for me

--- 7 kuy/The Coupon Code.js
+++ 7 kuy/The Coupon Code.js
@@ -1,3 +1,5 @@
 function checkCoupon(enteredCode, correctCode, currentDate, expirationDate){
     return enteredCode === correctCode && Date.parse(currentDate) <= Date.parse(expirationDate);
+    //enteredCode === correctCode - here we compare the arrays
+    //Date.parse(currentDate) is is representation of a current date(month - year - day) and if it is less than expirationDate it should return true otherwise false
   }
