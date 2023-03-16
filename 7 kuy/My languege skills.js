ec3d511a36c9855014c1442fc3cd971e5c8726d9
onyxgeek <onyxpeek@gmail.com>
2023-03-16 11:40:17 +0200

    upd: update the solution with coms

--- 7 kuy/My languege skills.js
+++ 7 kuy/My languege skills.js
@@ -1,19 +1,21 @@
-let results ={"C++": 50, "ASM": 10, "Haskell": 20} ,
+let results ={"Hindi": 60, "Dutch" : 93, "Greek": 71} ,
     arr = [],
     arrMerged = [],
     arrFinal = [];
-
+//Push in the new array key and value where value is bigger than 60
 for (let key in results) {
    if(results[key] >= 60) {
     arr.push([key, results[key]]);
    }
   };
-
+//Sort this array by the number in descending order
   arr.sort(function(a, b) {
     return b[1] - a[1]
   });
 
+  //Here a merged 2D array in 1D array
   arrMerged = arr.flat(1);
+  //Here I remove all numbers form array
   arrFinal = arrMerged.filter(x => isNaN(x))
  console.log(arrFinal);
 
