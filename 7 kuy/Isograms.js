72215282119bb5b160899216866dbfcc9d88acd2
onyxgeek <onyxpeek@gmail.com>
2023-03-17 10:23:55 +0200

    upd: update the task 7kuy Isograms with comments

--- 7 kuy/Isograms.js
+++ 7 kuy/Isograms.js
@@ -1,11 +1,16 @@
 let str = "vicccctoriaAaa",
+//Used split to turn string into array
     arr = str.split(""),
     arrLow = [];
+    //Here I pushed to a new array each index with .toLowerCase()
     for (let index = 0; index < arr.length; index++) {
         arrLow.push(arr[index].toLowerCase());
       }
 
+    //Here I created new set from an array where same characters will be deleted
     let  arrSet = new Set (arrLow),
+
+    //Here I turn Set which is an object into array
         arrFinal = Array.from(arrSet);
 
       console.log(arrFinal)
