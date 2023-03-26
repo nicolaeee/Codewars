ab85d7bbae87cfed681323dc8ca8ca0b04ed8735
onyxgeek <onyxpeek@gmail.com>
2023-03-26 23:41:42 +0300

    feat: add some solution for Array Deep Count 6kuy problem

--- 6 kuy/Array Deep Count.js
+++ 6 kuy/Array Deep Count.js
@@ -1,2 +1,14 @@
-arr = [[[]], [4[5]]];
-//should return sum of all arrays and evean nested arrays the say here should be recrusionnn
+let array = [1, 2, [3, 4, [5]]]
+console.log(array.flat(Infinity).length)
+function countElements(arr) {
+    let count = 0;
+    for (let i = 0; i < arr.length; i++) {
+      count++;
+      if (Array.isArray(arr[i])) {
+        count += countElements(arr[i]);
+      }
+    }
+    return count;
+  }
+
+  console.log(countElements(array))
