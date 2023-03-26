740a76c0a425a8c5beb74ecbfaa697a6ec803652
onyxgeek <onyxpeek@gmail.com>
2023-03-26 23:48:22 +0300

    fix: fix the final solution and commit on the solution

--- 6 kuy/Array Deep Count.js
+++ 6 kuy/Array Deep Count.js
@@ -1,12 +1,14 @@
 let array = [1, 2, [3, 4, [5]]]
-let count = a.length;
-for(let i = 0; i < a.length; i++){
-  if(Array.isArray(a[i])){
-    count = count + deepCount(a[i])
+function deepCount(a){
+    let count = a.length;
+    for(let i = 0; i < a.length; i++){
+      if(Array.isArray(a[i])){
+        count = count + deepCount(a[i])
+      }
+    }
+    return count;
   }
-  return count;
-}
 
 
 
-  console.log(countElements(array))
+  console.log(deepCount(array))
