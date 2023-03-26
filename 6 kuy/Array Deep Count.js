e0540949030c6c8b6281b05908ba243175e40a97
onyxgeek <onyxpeek@gmail.com>
2023-03-26 23:46:52 +0300

    fix: add another more ez solution for this kata

--- 6 kuy/Array Deep Count.js
+++ 6 kuy/Array Deep Count.js
@@ -1,14 +1,12 @@
 let array = [1, 2, [3, 4, [5]]]
-console.log(array.flat(Infinity).length)
-function countElements(arr) {
-    let count = 0;
-    for (let i = 0; i < arr.length; i++) {
-      count++;
-      if (Array.isArray(arr[i])) {
-        count += countElements(arr[i]);
-      }
-    }
-    return count;
+let count = a.length;
+for(let i = 0; i < a.length; i++){
+  if(Array.isArray(a[i])){
+    count = count + deepCount(a[i])
   }
+  return count;
+}
+
+
 
   console.log(countElements(array))
