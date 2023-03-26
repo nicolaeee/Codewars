ab330e2320aeae868bafdce19274ea4255d6ef2e
onyxgeek <onyxpeek@gmail.com>
2023-03-26 17:55:13 +0300

    init: start another algorim task on 6 kuy about Fibonaci numbers

--- 4 kuy/tempCodeRunnerFile.js
+++ 4 kuy/tempCodeRunnerFile.js
@@ -1,26 +1,6 @@
-  function formatDuration (seconds) {
-    let arr = [],
-     arrFinal = [],
-     totalMinutes = Math.floor(seconds / 60),
-     second = seconds % 60,
-     hours = Math.floor(totalMinutes / 60),
-     minutes = totalMinutes % 60;
+function foo(a, b){
+    return a * b;
+}
 
-     if(seconds === 1 ){
-         arr.push(seconds + " second");
-          return arrFinal = arr.join(" ")
-     } else if(seconds >= 5 && seconds <60){
-         arr.push(seconds + " seconds");
-        return  arrFinal = arr.join(" ");
-     } else if(seconds === 60 ){
-         arr.push(minutes + " minute");
-         return arrFinal = arr.join(" ");
-     } else if(seconds === 60 && second === 1){
-       arr.push(minutes + " minute and "+ second + " second")
-       return arrFinal = arr.join(" ");
-     } else if(seconds === 60 && second > 1 && second < 60){
-       arr.push(minutes + " minute and "+ second + " seconds")
-       return arrFinal = arr.join(" ");
-     }
- };
- formatDuration(62);
+const bar = foo.bind(null, 2);
+bar(2);
---
+++ 4 kuy/test.js
@@ -0,0 +1,5 @@
+printM();
+
+function printM(){
+    console.log("h")
+}
