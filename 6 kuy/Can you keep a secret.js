0aa6cc8add4859db04446aaff9ef1f81b6aa3fa7
onyxgeek <onyxpeek@gmail.com>
2023-03-28 23:04:08 +0300

    init: start the task

--- 6 kuy/Can you keep a secret.js
+++ 6 kuy/Can you keep a secret.js
@@ -1,10 +1,3 @@
 function createSecretHolder(secret) {
-    return {
-      getSecret: function() {
-        return secret;
-      },
-      setSecret: function(newSecret) {
-        secret = newSecret;
-      }
-    };
+
   }
