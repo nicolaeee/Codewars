a872f97162167c3586ef42ec772fd346143dfda7
onyxgeek <onyxpeek@gmail.com>
2023-03-28 23:11:15 +0300

    feat: finish the main task

--- 6 kuy/Can you keep a secret.js
+++ 6 kuy/Can you keep a secret.js
@@ -1,3 +1,16 @@
-function createSecretHolder(secret) {
 
+//I should impliment a function that takes a n value and set it to in an object
+function createSecretHolder(secret) {
+  return{
+    setSecret: function(){
+      return secret;
+    },
+    getSecret: function(newSecret){
+      secret = newSecret;
+    }
   }
+  }
+  let obj = {};
+
+obj = createSecretHolder(8);
+obj.getSecret();
