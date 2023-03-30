cca8e1ea4fd280d7bbecca98657a4cce6c06d4d3
onyxgeek <onyxpeek@gmail.com>
2023-03-30 19:06:58 +0300

    feat: add solution

--- 6 kuy/Sorting by bits.js
+++ 6 kuy/Sorting by bits.js
@@ -1,15 +1,29 @@
-function intArrayToBitSum(arr) {
-    const bitSum = [];
-    for (let i = 0; i < arr.length; i++) {
-      let sum = 0;
-      for (let j = 31; j >= 0; j--) {
-        sum += (arr[i] >> j) & 1;
-      }
-      bitSum.push(sum);
+function sortByBit(arr) {
+  const bitArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    const bits = [];
+    for (let j = 31; j >= 0; j--) {
+      bits.push((arr[i] >> j) & 1);
     }
-    return bitSum;
+    const numOnes = bits.filter(bit => bit === 1).length;
+    bitArray.push({bits, numOnes, num: arr[i]});
   }
-  const arr = [9,4,5,3,5,7,2,56,8,2,6,8,0];
+  bitArray.sort((a, b) => {
+    if (a.numOnes < b.numOnes) return -1;
+    if (a.numOnes > b.numOnes) return 1;
+    if (a.num < b.num) return -1;
+    if (a.num > b.num) return 1;
+    return 0;
+  });
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = bitArray[i].num;
+  }
+  return arr;
+}
 
 
-  console.log(intArrayToBitSum(arr)); // [1, 8, 3, 3, 5, 6, 9, 7]
+
+  const arr = [ 3, 8, 3, 6, 5, 7, 9, 1 ];
+
+
+  console.log(intArrayToSortedArray(arr)); // [1, 8, 3, 3, 5, 6, 9, 7]
