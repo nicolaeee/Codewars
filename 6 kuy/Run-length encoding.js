4cc3fc45eb72bb1d3f27e493584e1eb587834aff
onyxgeek <onyxpeek@gmail.com>
2023-04-02 22:33:30 +0300

    fix: add coments on solving the task

--- 6 kuy/Run-length encoding.js
+++ 6 kuy/Run-length encoding.js
@@ -1,13 +1,13 @@
 const str = 'hello world!';
-let count = 1;
-let result = [];
+let count = 1;//curent and default number of a character in str it can be less than 1
+let result = [];//create new array for final result
 
-for (let i = 0; i < str.length; i++) {
-  if (str[i] === str[i + 1]) {
-    count++;
-  } else {
-    result.push([count, str[i]]);
-    count = 1;
+for (let i = 0; i < str.length; i++) {//iterete through each element of string
+  if (str[i] === str[i + 1]) {//check if the current element is the same as next
+    count++;//true, and the count as like the total number of str characters rises as many times as it is true
+  } else {//when come to false it push each character in a new array and the amount of it in str
+    result.push([count, str[i]]);//push the amount and character itself
+    count = 1;//put it back to default state and going back at i++ to itterate over and over again
   }
 }
 
