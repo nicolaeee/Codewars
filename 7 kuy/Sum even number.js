34f634cebb4f3564a014db16d69dc0b33891bbbf
onyxgeek <onyxpeek@gmail.com>
2023-04-05 14:35:22 +0300

    feat: add solution

--- 7 kuy/Sum even number.js
+++ 7 kuy/Sum even number.js
@@ -1,1 +1,14 @@
-sa
+let input = [4, 3, 1, 2, 5, 10, 6, 7, 9, 8] ;
+
+function sumEvenNumbers(seq) {
+    let sum = 0;
+    for (let i = 0; i < seq.length; i++) {
+      let num = seq[i];
+      if (Number.isInteger(num) && num % 2 === 0) {
+        sum += num;
+      }
+    }
+    return sum;
+  }
+
+console.log(sumEvenNumbers(input))
