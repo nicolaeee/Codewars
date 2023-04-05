1e27c39a5e85a59c6bf1696aaaf4f804bd116743
onyxgeek <onyxpeek@gmail.com>
2023-04-05 08:43:55 +0300

    feat: add some comments

--- 6 kuy/Closures and Scopes.js
+++ 6 kuy/Closures and Scopes.js
@@ -1,11 +1,16 @@
 function createFunctions(n) {
-    var callbacks = [];
+    var callbacks = []; // Создаем пустой массив функций
 
-    for (let i=0; i<n; i++) {
-      callbacks.push(function() {
-        return i;
+    for (let i=0; i<n; i++) { // Начинаем цикл, итерирующий от 0 до n
+      callbacks.push(function() { // Для каждой итерации цикла создаем новую функцию и добавляем ее в массив
+        return i; // Функция возвращает значение i на момент создания
       });
     }
 
-    return callbacks;
+    return callbacks; // Возвращаем массив из созданных функций
   }
+  /*For example when I do have a block of functions and I do need to use function in function or recrusion or somenthing
+  the nested function will adress to the external variable and if you use var we can get Closure Problem because on each iteration
+  var is the same values but when we use let , variabile will be like block model view and on each iteration of loop will create new variabile
+  with let and not stay on the same
+  */
