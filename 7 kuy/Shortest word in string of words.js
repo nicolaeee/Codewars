19277153affaa983025c23340d007add8c2ce258
onyxgeek <onyxpeek@gmail.com>
2023-04-06 16:42:39 +0300

    feat: add comments

--- 7 kuy/Shortest word in string of words.js
+++ 7 kuy/Shortest word in string of words.js
@@ -1,14 +1,15 @@
 function findShort(s) {
-    let arr = s.split(' ');
+    let arr = s.split(' ');//here I created an array word split by ' '
     let minLen = Infinity; // initialize with a very large value
 
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i].length < minLen) {
+    for (let i = 0; i < arr.length; i++) {//going through arr length
+        console.log(minLen)
+      if (arr[i].length < minLen) { // if nested element length is smaller than minLen, minLen gets the length of that element and so on
         minLen = arr[i].length;
       }
     }
 
-    return minLen;
+    return minLen;//return the value
   }
 
 let s = 'bitcoin take over the world maybe who knows perhaps';
