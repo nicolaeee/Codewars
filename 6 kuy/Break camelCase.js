91b17bfd254692f99e93e668c0e5148f9dbd1493
onyxgeek <onyxpeek@gmail.com>
2023-04-06 22:06:01 +0300

    feat: add coments

--- 6 kuy/Break camelCase.js
+++ 6 kuy/Break camelCase.js
@@ -1,16 +1,15 @@
 function solution(string) {
-    let arr = string.split(/(?=[A-Z])/);
+    let arr = string.split(/(?=[A-Z])/);//here I do split string into arr and separate by the caps letter
 
   if(string === string.toLowerCase()){
-    return string
+    return string //here I do return string if it is all alredy on lowerCase
   }
   else if(string === ''){
-    return '';
+    return ''; //here I return an empty string if it is empty
   }
   else {
-    return arr.join(' ');
+    return arr.join(' ');//here I do return an array and join it to string with separate each index with space as I should
   }
-
 }
 
 console.log(solution('camelCase'))
