acf804d30a7376fe494778ea944d985b1b64c3fc
onyxgeek <onyxpeek@gmail.com>
2023-04-06 15:39:09 +0300

    feat: add coments to the Get the middle char

--- 7 kuy/Get the middle charachter.js
+++ 7 kuy/Get the middle charachter.js
@@ -1,13 +1,13 @@
 function getMiddle(s)
 {
-  let arr = s.split(''),
-      len = arr.length,
-      midIndex = Math.floor(len/2);
-  if(len % 2 === 0){
-   return arr[midIndex - 1] + arr[midIndex];
+  let arr = s.split(''),//create array form string
+      len = arr.length,//put the length of arr in separate varbiabile
+      midIndex = Math.floor(len/2);//get the mid index of arr
+  if(len % 2 === 0){//if the length is even
+   return arr[midIndex - 1] + arr[midIndex];//return the middle two characters of even length of array
   }
   else {
-    return arr[midIndex]
+    return arr[midIndex]//return the middle of an array
   }
 }
 console.log(getMiddle("atel"))
