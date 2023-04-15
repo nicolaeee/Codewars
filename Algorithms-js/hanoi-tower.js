cd3498e27e6e5730b03cfae1d22bfd75c452995c
onyxgeek <onyxpeek@gmail.com>
2023-04-15 13:00:00 +0300

    feat: add solution to the hanoi-tower.js

---
+++ Algorithms-js/hanoi-tower.js
@@ -0,0 +1,19 @@
+/**
+ * Calculate turns number and time (in seconds) required
+ * to solve puzzle
+ *
+ * @param {Number} disks number of disks
+ * @param {Number} turnsSpeed speed (in turns/hour)
+ * @return {Object} object with props turns (number of turns)
+ * and seconds (time in seconds)
+ *
+ * @example
+ *
+ * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
+ *
+ */
+function calculateHanoi(disks, turnSpeed) {
+    const turns = 2 ** disks - 1;
+    seconds = Math.floor((2**disks - 1) / (turnSpeed / 3600));
+    return { turns, seconds };
+  }
