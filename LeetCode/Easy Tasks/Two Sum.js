d8971c6409bca949b38bddaf93523f790ed0423d
onyxgeek <onyxpeek@gmail.com>
2023-06-06 09:57:21 +0300

    feat: add solution with the hash table

--- LeetCode/Easy Tasks/Two Sum.js
+++ LeetCode/Easy Tasks/Two Sum.js
@@ -1,1 +1,43 @@
-c
+/*
+
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+*/
+
+
+var twoSum = function(nums, target) {
+ let map = new Map();
+
+ for(let i = 0; i < nums.length; i++){
+    let num1 = nums[i];
+    let num2 = target - num1;
+
+    if(map.has(num2)){
+        return [i, map.get(num2)]
+    }
+    map.set(num1, i)
+ }
+};
+
+let nums = [2, 7, 11, 15];
+
+twoSum(nums, 9)
