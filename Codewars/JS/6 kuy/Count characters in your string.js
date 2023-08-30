f751a8e9c7799114cde5c73565dec06bdf7e25e5
onyxgeek <onyxpeek@gmail.com>
2023-08-30 20:03:55 +0300

    feat: add the comments on the solution

--- Codewars/JS/6 kuy/Count characters in your string.js
+++ Codewars/JS/6 kuy/Count characters in your string.js
@@ -13,21 +13,33 @@ for(let i = 0; i < string.length; i++){
 console.log(histogram)
 
 function count(string) {
+    // Create an empty object to store the character count
     histogram = {};
-    for(let i = 0; i < string.length; i++){
-        let ch = string[i]
 
+    // Loop through each character in the input string
+    for(let i = 0; i < string.length; i++){
+        // Get the current character
+        let ch = string[i];
+
+        // Check if the character is not already a property in the histogram object
         if(!histogram[ch]){
+            // If not, initialize it as 0
             histogram[ch] = 0;
         }
-        histogram[ch]++
+
+        // Increment the count for the current character in the histogram
+        histogram[ch]++;
     }
 
+    // Check if the histogram object is empty (no unique characters)
     if(Object.keys(histogram).length === 0){
+        // If it's empty, return the empty histogram object
         return histogram ;
     }
     else
-        return histogram
-  }
+        // If it's not empty, return the histogram object with character counts
+        return histogram;
+}
+
 let waw = {};
 console.log(count(waw));
