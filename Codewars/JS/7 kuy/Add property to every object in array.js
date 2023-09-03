982e107a4fff97ad1294d838a4957a012c658c13
onyxgeek <onyxpeek@gmail.com>
2023-09-03 10:00:07 +0300

    feat: add the solution to the problem

---
+++ Codewars/JS/7 kuy/Add property to every object in array.js
@@ -0,0 +1,5 @@
+
+for(let i = 0; i < questions.length; i++){
+    questions[i].usersAnswer = null;
+}
+/* Here we add new property with key: value(usersAnswer: null) to every object in my array of objects*/
